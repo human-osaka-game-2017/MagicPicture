bd83a6b6c94947147dfe4dfeda47b6eeef5b1960
shirapi <bibi.jupiter@gmail.com>
2018-04-23 21:58:36 +0900

    カメラ切り替え Rayを飛ばす処理

---
+++ MagicPicture/Assets/Script/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] private GameObject FPSCamera;
+    [SerializeField] private GameObject TPSCamera;
+
+    private bool isFPSMode = false;
+    public bool IsFPSMode
+    {
+        get { return this.isFPSMode; }
+        private set { this.isFPSMode = value; }
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Z)) ChangeFTMode();
+    }
+
+    //FPSモード/TPSモード変更処理
+    void ChangeFTMode()
+    {
+
+        FPSCamera.SetActive(!FPSCamera.activeSelf);
+        TPSCamera.SetActive(!TPSCamera.activeSelf);
+        isFPSMode = !isFPSMode;
+    }
+}
---
+++ MagicPicture/Assets/Script/MagiCame.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagiCame : MonoBehaviour {
+
+    public LayerMask mask;
+
+    [SerializeField] private GameObject fourcs;
+
+    private GameObject m_GameManager = null;
+    private GameObject m_Player;
+    private Vector3 m_Direction;
+    private Ray m_Ray;
+
+    private void Awake()
+    {
+        m_GameManager   = GameObject.Find("GM");
+        m_Player        = GameObject.Find("Player");
+        m_Ray           = new Ray(transform.position, transform.forward.normalized);
+    }
+
+    void Start () {
+       
+    }
+
+    void Update () {
+
+        //レイ表示
+        Debug.DrawRay(m_Ray.origin, m_Ray.direction, Color.red);
+
+        if (m_GameManager.GetComponent<GameManager>().IsFPSMode)
+        {
+            // Rayが衝突したコライダーの情報を得る
+            RaycastHit hit;
+
+            // 衝突したオブジェクトの色を赤に変える
+            bool rayIsCollided = Physics.Raycast(m_Ray, out hit, 10000, mask);
+
+            if(rayIsCollided)
+            {
+                hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+            }
+
+            if (/*!player.m_IsMoving &&*/Input.GetKeyDown(KeyCode.Space))
+            {
+                // Rayが衝突したかどうか
+                if (rayIsCollided)
+                {
+                    // Rayの原点から衝突地点までの距離を得る
+                    float dis = hit.distance;
+                }
+            }
+        }
+    }
+}

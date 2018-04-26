54569e29eeb95fd48292be34f0f1777bb41dac52
DESKTOP-O72TMG5\robit <robito1125@gmail.com>
2018-04-26 12:25:17 +0900

    no message

---
+++ MagicPicture/Assets/Gimmick/Gimmick_Door/DoorTrigger_M.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorTrigger_M : MonoBehaviour {
+
+    GameObject doorR;
+    GameObject doorL;
+
+    // Use this for initialization
+    void Start () {
+        doorR = GameObject.Find("doorR");
+        doorL = GameObject.Find("doorL");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "b")  //タグをマジカメのタグに置き換える
+        doorL.transform.Translate(new Vector3(-0.5f, 0, 0));
+        doorR.transform.Translate(new Vector3(0.5f, 0, 0));
+    }
+}

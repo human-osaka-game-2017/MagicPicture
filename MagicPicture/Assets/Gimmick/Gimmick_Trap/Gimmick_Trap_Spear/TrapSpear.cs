4d8b5a9c26311762a7c9a32e0183ee9ab9d81165
DESKTOP-O72TMG5\robit <robito1125@gmail.com>
2018-05-02 15:47:10 +0900

    test

--- MagicPicture/Assets/Gimmick/Gimmick_Trap/Gimmick_Trap_Spear/TrapSpear.cs
+++ MagicPicture/Assets/Gimmick/Gimmick_Trap/Gimmick_Trap_Spear/TrapSpear.cs
@@ -9,11 +9,6 @@ public class TrapSpear : MonoBehaviour {
 
     private GameObject spearTrigger;
 
-    public int x;
-    public int y;
-    public int z;
-
-
 
     public float dropSpeed = 5f;
 
@@ -23,26 +18,37 @@ public class TrapSpear : MonoBehaviour {
         //x = 0;
         //y = 0;
         //z = 0;
-	}
+        GoSpear();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        if (spearTrigger.GetComponent<SpearTrigger>().SpearOn == true)
-        {
-            //spear = (GameObject)Instantiate(SpearPrefab);
-            Instantiate(SpearPrefab, transform.position, Quaternion.identity);
-            spear.GetComponent<Spear>().Shot(new Vector3(0, 0, 0));
+       
+           
 
-            spear.transform.Translate(0, this.dropSpeed, 0);
-            if (spear.transform.position.y < 3f)         //
-            {
+            //spear = (GameObject)Instantiate(SpearPrefab);
+            
+            
+
+            //spear.transform.Translate(0, this.dropSpeed, 0);
+            //if (spear.transform.position.y < 3f)         //
+            //{
                 //Destroy(this);
-            }
+            //}
 
             Debug.Log("スピアー発射");
+        
+
+        
+
+    }
+    void GoSpear()
+    {
+        if (spearTrigger.GetComponent<SpearTrigger>().SpearOn == true)
+        {
+            Instantiate(SpearPrefab, transform.position, Quaternion.identity);
         }
     }
-    
-   
-    
+
+
 }

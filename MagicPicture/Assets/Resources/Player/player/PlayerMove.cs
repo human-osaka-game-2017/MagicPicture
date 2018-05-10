4f538b28341d554d59f580a69237a9d991f79b90
FunaKatu <katuto.funaoka2000@gmail.com>
2018-05-10 17:06:23 +0900

    作業中

--- MagicPicture/Assets/Resources/Player/player/PlayerMove.cs
+++ MagicPicture/Assets/Resources/Player/player/PlayerMove.cs
@@ -23,14 +23,10 @@ public class PlayerMove : MonoBehaviour {
     
     // Use this for initialization
     void Start () {
-<<<<<<< HEAD
         TitleScene meteortmp = Instantiate(titleScene);
         meteortmp.gameObject.SetActive(true);
-=======
->>>>>>> 2f413c416e5daff3c06b2a9b1012b1e78622001c
     }
 
-<<<<<<< HEAD
     // Update is called once per frame
     void Update()
     {
@@ -40,44 +36,32 @@ public class PlayerMove : MonoBehaviour {
             GetComponent<Renderer>().material = _material[0];
         }
 
-        if (sceneDivergence == (int)Scene.e_LoadGame) {
+        if (sceneDivergence == (int)Scene.e_LoadGame)
+        {
             ExecuteLoad();
             sceneDivergence = (int)Scene.e_Gameing;
-=======
-        // TPS時
-        if (!this.GetComponent<CameraSystem>().IsFPSMode) {
-            TPSMove();
-        }
-
-        // FPS時
-        if (this.GetComponent<CameraSystem>().IsFPSMode) {
-            FPSMove();
->>>>>>> 2f413c416e5daff3c06b2a9b1012b1e78622001c
         }
     }
     
-    void FixedUpdate()
-    {
-<<<<<<< HEAD
-        if (sceneDivergence == (int)Scene.e_Gameing) {
+    //void FixedUpdate()
+    //{
+    //    if (sceneDivergence == (int)Scene.e_Gameing) {
 
-            // TPS時
-            if (!fCameraSystem.changeMode) {
-                ForwardAndBack();
-            }
+    //        // TPS時
+    //        if (!fCameraSystem.changeMode) {
+    //            ForwardAndBack();
+    //        }
 
-            // FPS時
-            if (fCameraSystem.changeMode) {
-                ForwardAndBack();
-                RightAndLeft();
-            }
-=======
-        if (changeVector != 0) {
-            transform.position += transform.up * playerSpeed * changeVector;
-            Camera.main.transform.Translate(transform.up * playerSpeed * changeVector);
->>>>>>> 2f413c416e5daff3c06b2a9b1012b1e78622001c
-        }
-    }
+    //        // FPS時
+    //        if (fCameraSystem.changeMode) {
+    //            ForwardAndBack();
+    //            RightAndLeft();
+    //        }
+    //    if (changeVector != 0) {
+    //        transform.position += transform.up * playerSpeed * changeVector;
+    //        Camera.main.transform.Translate(transform.up * playerSpeed * changeVector);
+    //    }
+    //}
 
 
     //===========
@@ -99,8 +83,6 @@ public class PlayerMove : MonoBehaviour {
     //===========
     void RightAndLeft()
     {
-<<<<<<< HEAD
-=======
         Vector3 movement = Vector3.zero;
 
         if (Input.GetKey("w")) {
@@ -109,13 +91,12 @@ public class PlayerMove : MonoBehaviour {
         if (Input.GetKey("s")) {
             movement += transform.up * playerSpeed;
         }
->>>>>>> 2f413c416e5daff3c06b2a9b1012b1e78622001c
-        if (Input.GetKey("a")) {
-            movement -= transform.right * playerSpeed;
-        }
-        if (Input.GetKey("d")) {
-            movement += transform.right * playerSpeed;
-        }
+        //if (Input.GetKey("a")) {
+        //    movement -= transform.right * playerSpeed;
+        //}
+        //if (Input.GetKey("d")) {
+        //    movement += transform.right * playerSpeed;
+        //}
 
         this.transform.Translate(movement);
         Camera.main.transform.Translate(movement);

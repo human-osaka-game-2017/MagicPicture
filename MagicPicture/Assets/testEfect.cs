72940b485348a2211751e3c678556b36294b223c
DESKTOP-O72TMG5\robit <robito1125@gmail.com>
2018-06-07 15:29:09 +0900

    stage

---
+++ MagicPicture/Assets/testEfect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testEfect : MonoBehaviour
+{
+    ParticleSystem effect;
+    public 
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void E_damaage()
+    {
+
+    }
+}
